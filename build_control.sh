e349b704644d6938cf069d41e5afeae8e2401c47
amov-nuc-gpu <fatmoonqyp@126.com>
2025-04-01 18:23:56 +0800

    update simulation

--- build_control.sh
+++ build_control.sh
@@ -6,3 +6,7 @@ catkin_make --source General_Module/sunray_common --build build/sunray_common
 catkin_make --source General_Module/sunray_uav_control --build build/sunray_uav_control
 # 编译sunray_tutorial模块
 catkin_make --source General_Module/sunray_tutorial --build build/sunray_tutorial
+# 编译sunray_simulator模块
+catkin_make --source Simulation/sunray_simulator --build build/sunray_simulator
+# 编译simulator_utils模块
+catkin_make --source Simulation/simulator_utils --build build/simulator_utils
