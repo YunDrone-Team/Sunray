9832e87b134379fc64df5af6b3e713f46c63b90c
Yao <925935221@qq.com>
2025-04-18 21:21:27 +0800

    update

--- General_Module/sunray_TV/src/auto_waypoint.cpp
+++ General_Module/sunray_TV/src/auto_waypoint.cpp
@@ -42,10 +42,10 @@ public:
         px4_state_sub = nh.subscribe<sunray_msgs::PX4State>(topic_prefix + "/sunray/px4_state", 10, &auto_waypoint::px4_state_callback, this);
         uav_state_sub = nh.subscribe<sunray_msgs::UAVState>(topic_prefix + "/sunray/uav_state", 10, &auto_waypoint::uav_state_callback, this);
 
-        plan_points.push_back(tuple<float, float, float>(5, 3, 1));
-        plan_points.push_back(tuple<float, float, float>(-3, -5, 1));
-        plan_points.push_back(tuple<float, float, float>(-10, 0, 1));
-        plan_points.push_back(tuple<float, float, float>(1, 0, 1));
+        plan_points.push_back(tuple<float, float, float>(1, 1, 1));
+        plan_points.push_back(tuple<float, float, float>(1, -, 1));
+        plan_points.push_back(tuple<float, float, float>(-1, -1, 1));
+        plan_points.push_back(tuple<float, float, float>(0, 0, 1));
 
         state = 0;
         next_step = 0;
