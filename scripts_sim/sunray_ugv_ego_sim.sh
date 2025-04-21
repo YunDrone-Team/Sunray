5bef2e5633ebefdfc9671b309fd142f672b64f37
Yao <925935221@qq.com>
2025-04-21 21:34:32 +0800

    add ugv sim scripts

--- scripts_sim/sunray_ugv_ego_sim.sh
+++ scripts_sim/sunray_ugv_ego_sim.sh
@@ -6,5 +6,5 @@
 gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator sunray_sim_1ugv.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3.0; roslaunch sunray_ugv_control ugv_control_sim.launch; exec bash"' \
 
-gnome-terminal --window -e 'bash -c "sleep 2.0; roslaunch sunray_TV sunray_ego_sim.launch; exec bash"' \
+gnome-terminal --window -e 'bash -c "sleep 2.0; roslaunch sunray_TV sunray_ego_sim_ugv.launch; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_map_generator map_generator.launch map_name:=planning_test rviz_enable:=false; exec bash"' \
