10ffa0ad53de7005015b010245f0ccd50e676f7c
Yao <925935221@qq.com>
2025-04-30 10:06:14 +0800

    修改ugv模型

--- scripts_sim/auto_land.sh
+++ scripts_sim/auto_land.sh
@@ -9,7 +9,7 @@
 
 #!/bin/bash
 # start gazebo
-gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator sunray_sim_arcuo.launch; exec bash"' \
+gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator sunray_sim_aruco.launch; exec bash"' \
 --tab -e 'bash -c "sleep 5.0; roslaunch sunray_uav_control external_fusion.launch external_source:=2; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control sunray_control_node.launch uav_id:=1; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control terminal_control.launch uav_id:=1; exec bash"' \
