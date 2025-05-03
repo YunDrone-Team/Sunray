fd1c3510201a3b919696c830817b024143e6a052
Yuhua.Qi <fatmoonqyp@126.com>
2025-05-03 19:30:08 +0800

    update

--- scripts_sim/uav_control_sim.sh
+++ scripts_sim/uav_control_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# start gazebo
-gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator world_test.launch; exec bash"' \
---tab -e 'bash -c "sleep 8.0; roslaunch sunray_simulator sunray_sim_1uav.launch vehicle:=sunray150_with_mid360; exec bash"' \
---tab -e 'bash -c "sleep 5.0; roslaunch sunray_uav_control external_fusion.launch external_source:=2; exec bash"' \
+# 脚本：无人机控制测试脚本
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 5.0; roslaunch sunray_simulator sunray_sim_1uav.launch vehicle:=sunray150_with_mid360; exec bash"' \
+--tab -e 'bash -c "sleep 8.0; roslaunch sunray_uav_control external_fusion.launch external_source:=2; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control sunray_control_node.launch uav_id:=1; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control terminal_control.launch uav_id:=1; exec bash"' \
 
