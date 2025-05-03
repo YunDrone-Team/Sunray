48ecc4af730c955ff4d68f1ed2bab31e91801fb5
Yuhua.Qi <fatmoonqyp@126.com>
2025-05-03 19:30:08 +0800

    update

--- scripts_sim/sunray_takeoff_hover_land.sh
+++ scripts_sim/sunray_takeoff_hover_land.sh
@@ -1,4 +1,6 @@
 
+#!/bin/bash
+# 脚本：起飞降落demo
 gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator sunray_sim_1uav.launch; exec bash"' \
 --tab -e 'bash -c "sleep 8.0; roslaunch sunray_uav_control external_fusion.launch external_source:=2 enable_rviz:=true; exec bash"' \
 --tab -e 'bash -c "sleep 5.0; roslaunch sunray_uav_control sunray_control_node.launch uav_id:=1; exec bash"' \
