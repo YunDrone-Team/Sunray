995ea406eecd04be99a6cb4aa811439966832400
Yao <925935221@qq.com>
2025-05-08 16:58:34 +0800

    增加mid360插件 增加ego仿真脚本

--- scripts_sim/uav_control_sim.sh
+++ scripts_sim/uav_control_sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 脚本：无人机控制测试脚本
 gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator sunray_sim_1uav.launch vehicle:=sunray150; exec bash"' \
---tab -e 'bash -c "sleep 8.0; roslaunch sunray_uav_control external_fusion.launch external_source:=2; exec bash"' \
+--tab -e 'bash -c "sleep 10.0; roslaunch sunray_uav_control external_fusion.launch external_source:=2; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control sunray_control_node.launch uav_id:=1; exec bash"' \
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_simulator sim_rviz.launch; exec bash"' \
 # --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control terminal_control.launch uav_id:=1; exec bash"' \
