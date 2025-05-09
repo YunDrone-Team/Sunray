cc75ff54cbe97f84080213a9adf9ae9567f18d76
Yao <925935221@qq.com>
2025-05-09 16:15:04 +0800

    修复仿真时间问题

--- scripts_exp/demo_auto_land.sh
+++ scripts_exp/demo_auto_land.sh
@@ -7,6 +7,6 @@ gnome-terminal --window -e 'bash -c "roslaunch sunray_uav_control sunray_mavros_
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control terminal_control.launch uav_id:=1; exec bash"' \
 
 gnome-terminal --window -e 'bash -c "sleep 3.0; roslaunch sunray_tutorial landmark_detection.launch; exec bash"' \
---tab -e 'bash -c "sleep 5.0; rqt_image_view; exec bash"' \
+--tab -e 'bash -c "sleep 5.0; rosrun rqt_image_view rqt_image_view; exec bash"' \
 --tab -e 'bash -c "sleep 8.0; roslaunch sunray_tutorial auto_land_by_pose.launch; exec bash"' \
 # --tab -e 'bash -c "sleep 8.0; roslaunch sunray_tutorial auto_land.launch; exec bash"' \
--- scripts_exp/demo_follow_tag.sh
+++ scripts_exp/demo_follow_tag.sh
@@ -5,5 +5,5 @@ gnome-terminal --window -e 'bash -c "roslaunch sunray_uav_control sunray_mavros_
 --tab -e 'bash -c "sleep 2.0; roslaunch sunray_uav_control terminal_control.launch uav_id:=1; exec bash"' \
 
 gnome-terminal --window -e 'bash -c "sleep 3.0; roslaunch sunray_tutorial qrcode_detection_front.launch; exec bash"' \
---tab -e 'bash -c "sleep 5.0; rqt_image_view; exec bash"' \
+--tab -e 'bash -c "sleep 5.0; rosrun rqt_image_view rqt_image_view; exec bash"' \
 --tab -e 'bash -c "sleep 8.0; roslaunch sunray_tutorial follow_a_tag.launch; exec bash"' 
