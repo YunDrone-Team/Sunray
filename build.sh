2835bd30190a805dffb5e0e5475e640923f9f1ce
Yao <925935221@qq.com>
2025-05-16 10:43:54 +0800

    修复仿真地图问题

--- build.sh
+++ build.sh
@@ -16,9 +16,6 @@ catkin_make --source General_Module/sunray_planner_utils --build build/sunray_pl
 catkin_make --source Comunication_Module/sunray_communication_bridge --build build/sunray_communication_bridge
 # 编译sunray_media模块
 catkin_make --source General_Module/sunray_media --build build/sunray_media
-# 编译sunray_ugv_control模块
-catkin_make --source General_Module/sunray_ugv_control --build build/sunray_ugv_control
-
 ## 注意：ego-planner与FUEL不能同时编译 会存在一些链接上的问题
 # 编译ego-planner-swarm模块
 catkin_make --source External_Module/ego-planner-swarm --build build/ego-planner
@@ -30,12 +27,9 @@ catkin_make --source Simulation/simulator_utils --build build/simulator_utils
 catkin_make --source Simulation/gazebo_plugin --build build/gazebo_plugin
 # 编译sunray_formation模块
 catkin_make --source sunray_formation --build build/sunray_formation
+
+# ugv需要octomap、serial相关以依赖
+# 编译sunray_ugv_control模块
+# catkin_make --source General_Module/sunray_ugv_control --build build/sunray_ugv_control
 # 编译turn_on_wheeltec_robot模块
-catkin_make --source External_Module/turn_on_wheeltec_robot --build build/turn_on_wheeltec_robot
-
-# catkin_make --source sunray_msgs --build build/sunray_msgs
-# catkin_make --source sunray_comm --build build/sunray_comm
-
-# catkin_make --source turn_on_wheeltec_robot --build build/turn_on_wheeltec_robot
-# catkin_make --source sunray_wheeltec_robot --build build/sunray_wheeltec_robot
-# catkin_make --source sunray_ugv_planning --build build/sunray_ugv_planning
+# catkin_make --source External_Module/turn_on_wheeltec_robot --build build/turn_on_wheeltec_robot
