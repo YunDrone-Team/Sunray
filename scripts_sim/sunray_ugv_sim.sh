3de510527572d33bd286b127088a04ede500afa9
heh666 <heh66@222.com>
2025-05-20 20:01:58 -0700

    ugv仿真脚本

---
+++ scripts_sim/sunray_ugv_sim.sh
@@ -0,0 +1,2 @@
+gnome-terminal --window -e 'bash -c "roslaunch sunray_simulator sunray_sim_1ugv.launch rviz_enable:=false; exec bash"' \
+--tab -e 'bash -c "sleep 3.0; roslaunch sunray_ugv_control ugv_control_sim.launch; exec bash"' \
