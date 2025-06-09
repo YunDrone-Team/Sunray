9e9234785ed5cde2521aa4fd2e4d4426264e5b6f
Yao <925935221@qq.com>
2025-06-09 15:12:01 +0800

    fix

--- scripts_exp/uav_formation.sh
+++ scripts_exp/uav_formation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-id = 1
-num = 3
+id=1
+num=3
 if [ -n "$1" ]; then
   id=$(($1))
 fi
@@ -14,5 +14,5 @@ gnome-terminal --window -e "bash -c \"roslaunch sunray_uav_control sunray_mavros
 
 gnome-terminal --window -e "bash -c \"sleep 3.0; roslaunch sunray_orca orca_ugv.launch agent_id:=${id} agent_num:=${num}; exec bash\"" \
                 --tab -e "bash -c \"sleep 5.0; roslaunch sunray_formation formation_single_ugv.launch agent_id:=${id}  agent_num:=${num}; exec bash\"" \
-                --tab -e "bash -c \"sleep 5.0; roslaunch sunray_communication_bridge sunray_communication_bridge.launch ugv_id:=1 ugv_experiment_num:=${num}; exec bash\"" \
+                --tab -e "bash -c \"sleep 5.0; roslaunch sunray_communication_bridge sunray_communication_bridge.launch ugv_id:=${id} ugv_experiment_num:=${num}; exec bash\"" \
 
