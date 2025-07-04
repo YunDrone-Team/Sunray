ff79f149ba8d05f5e01878318aa878ce580ac959
lceLemon <925935221@qq.com>
2025-07-04 08:56:50 +0000

    change
    Signed-off-by: lceLemon <925935221@qq.com>

--- scripts_exp/uav_control.sh
+++ scripts_exp/uav_control.sh
@@ -1,4 +1,4 @@
 
 gnome-terminal --window -e 'bash -c "roslaunch sunray_uav_control sunray_mavros_exp.launch; exec bash"' \
---tab -e 'bash -c "sleep 8.0; roslaunch sunray_uav_control external_fusion.launch external_source:=3 enable_rviz:=true; exec bash"' \
+--tab -e 'bash -c "sleep 8.0; roslaunch sunray_uav_control external_fusion.launch external_source:=3 enable_rviz:=true server:=192.168.25.27; exec bash"' \
 --tab -e 'bash -c "sleep 5.0; roslaunch sunray_uav_control sunray_control_node.launch uav_id:=1; exec bash"' \
