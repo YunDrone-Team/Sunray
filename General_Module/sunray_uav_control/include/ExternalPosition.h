d89de832e57b20b8caeef35bf135c725ea9734d1
Ray <1911542613@qq.com>
2025-08-19 10:05:14 +0800

    回退到之前的更改

--- General_Module/sunray_uav_control/include/ExternalPosition.h
+++ General_Module/sunray_uav_control/include/ExternalPosition.h
@@ -83,7 +83,6 @@ public:
     
     sunray_msgs::ExternalOdom external_odom;                // 声明一个自定义话题 - sunray_msgs::ExternalOdom
     sensor_msgs::Range distance_sensor;                     // 距离传感器原始数据
-    nav_msgs::Odometry msg_odom;                           // 声明一个自定义话题 - nav_msgs::Odometry
 
     void init(ros::NodeHandle &nh, int external_source = 0, std::string source_topic_name = "Odometry", bool range_sensor = false)
     {
@@ -283,21 +282,6 @@ public:
         {
             external_odom.position[2] = distance_sensor.range;
         }
-
-        msg_odom.pose.pose.position.x = msg->pose.pose.position.x;
-        msg_odom.pose.pose.position.y = msg->pose.pose.position.y;
-        msg_odom.pose.pose.position.z = msg->pose.pose.position.z;
-        msg_odom.pose.pose.orientation.x = q.getX();
-        msg_odom.pose.pose.orientation.y = q.getY();
-        msg_odom.pose.pose.orientation.z = q.getZ();
-        msg_odom.pose.pose.orientation.w = q.getW();
-        msg_odom.twist.twist.linear.x = msg->twist.twist.linear.z;
-        msg_odom.twist.twist.linear.y = -msg->twist.twist.linear.x;
-        msg_odom.twist.twist.linear.z = -msg->twist.twist.linear.y;
-        msg_odom.twist.twist.angular.x = msg->twist.twist.angular.z;
-        msg_odom.twist.twist.angular.y = -msg->twist.twist.angular.x;
-        msg_odom.twist.twist.angular.z = -msg->twist.twist.angular.y;
-        msg_odom.header.stamp = ros::Time::now();
     }
 
     void VelCallback(const geometry_msgs::TwistStamped::ConstPtr &msg)
