037a6e15696f99a13909ca8a2417e1d6e7f8df1c
Ray <1911542613@qq.com>
2025-08-25 17:16:01 +0800

    适配地面站

--- build.sh
+++ build.sh
@@ -11,14 +11,14 @@ catkin_make --source General_Module/sunray_tutorial --build build/sunray_tutoria
 # 编译sunray_simulator模块
 catkin_make --source Simulation/sunray_simulator --build build/sunray_simulator
 # 编译sunray_planner模块
-catkin_make --source General_Module/sunray_planner_utils --build build/sunray_planner_utils
+# catkin_make --source General_Module/sunray_planner_utils --build build/sunray_planner_utils
 #编译sunray_communication_bridge模块
 catkin_make --source Comunication_Module/sunray_communication_bridge --build build/sunray_communication_bridge
 # 编译sunray_media模块
 # catkin_make --source General_Module/sunray_media --build build/sunray_media
 ## 注意：ego-planner与FUEL不能同时编译 会存在一些链接上的问题
 # 编译ego-planner-swarm模块
-catkin_make --source External_Module/ego-planner-swarm --build build/ego-planner
+# catkin_make --source External_Module/ego-planner-swarm --build build/ego-planner
 # 编译FUEL模块
 # catkin_make --source External_Module/FUEL --build build/FUEL
 # 编译simulator_utils模块
