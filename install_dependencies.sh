ff4a23bda1964dc24f50561e458c4a5b39b503e3
yuanhai_zou <1363655341@qq.com>
2025-09-08 08:42:03 +0000

    add tmux config in install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,3 +5,6 @@ sudo apt-get install ros-noetic-velodyne-gazebo-plugins* -y
 sudo apt-get install libignition-math* -y
 sudo apt-get install libyaml-cpp-dev -y
 sudo apt-get install libsfml-dev -y
+
+sudo apt install tmux
+tail -n 1 ~/.tmux.conf | grep -q "set -g mouse on" || echo 'set -g mouse on' | tee -a ~/.tmux.conf
