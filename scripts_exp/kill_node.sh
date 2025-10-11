3c733f598477c83296bd3a03d2940eb80bedd331
yundrone <yundrone>
2025-10-11 02:03:52 +0000

    Modify the kill_node.sh script to make it universal.

--- scripts_exp/kill_node.sh
+++ scripts_exp/kill_node.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+tmux kill-session -t sunray_tmux
+rostopic pub -1 /baton/stereo3_ctrl system_ctrl/algo_ctrl "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+algo_enable: false
+algo_reboot: false
+algo_reset: false"
+
 # ROS节点白名单（用空格分隔）
 WHITELIST="rosout rosmaster /sunray_communication_bridge"
 
