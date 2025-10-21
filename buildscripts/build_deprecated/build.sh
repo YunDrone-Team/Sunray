1b0f350fc01f818dc98dbef7bf7307244014b22f
Qiu <pingkaiqiu@163.com>
2025-10-21 17:20:35 +0800

    add build sunray_fmt_control in tui

--- buildscripts/build_deprecated/build.sh
+++ buildscripts/build_deprecated/build.sh
@@ -4,6 +4,8 @@
 catkin_make --source General_Module/sunray_common --build build/sunray_common
 # 编译sunray_uav_control模块
 catkin_make --source General_Module/sunray_uav_control --build build/sunray_uav_control
+# 编译sunray_fmt_control模块
+catkin_make --source General_Module/sunray_fmt_control --build build/sunray_fmt_control
 # 编译vrpn_client_ros功能包
 catkin_make --source External_Module/vrpn_client_ros --build build/vrpn_client_ros
 # 编译sunray_tutorial模块
